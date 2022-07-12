b93c332913b8b7790d10db2e32a4c06ac87eb1f8
Night-Cheza <leila.kassi@outlook.com>
2022-07-12 00:15:24 -0600

    created facts and fact files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+const catFacts = {
+  facts: [],
+  id: "",
+  text: ""
+}
 
 function App() {
+  const [displayFact, setDisplayFact] = useState(false);
+
+  const showFact = () => {    
+    setDisplayFact(true);
+  };
+
+  const dontShowFact = () => {
+    setDisplayFact(false);
+  };
+
+  if(displayFact) {
+    
+  }
+
   return (
     <div className="App">
-      
+      <p>Would you like to know a fact about cats?</p>
+      <button onClick = {showFact}>YES</button>
+      <button onClick = {dontShowFact}>NO</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Facts.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+
+const Facts = (props) => {
+    useState();
+
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Facts;
